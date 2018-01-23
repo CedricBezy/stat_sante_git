82417d7c7ac1faf5d7a71a7b0731217802a0db60
CedricBezy <cedric.bezy@gmail.com>
2018-01-23 15:29:52 +0100

    "import data" ==> "clean data"

--- code/clean_data.R
+++ code/clean_data.R
@@ -55,21 +55,21 @@ couples <- couples %>%
     dplyr::mutate(
         ## Relevel
         enfant = factor(enfant,
-                        levels = c(1, 0)),
+                        levels = c(1, 0),
+                        labels = c("Oui", "Non")),
         dconsultation = as.Date(dconsultation, format = '%d/%m/%Y'),
         dconception = as.Date(dconception, format = '%d/%m/%Y'),
         ddn = as.Date(ddn, format = '%d/%m/%Y'),
-        diplome_h = factor(diplome_h,
-                           levels = c('Bac-', 'Bac', 'Bac+'),
-                           labels = c(-1, 0, 1)),
-        diplome_f = factor(diplome_f,
-                           levels = c('Bac-', 'Bac', 'Bac+'),
-                           labels = c(-1, 0, 1)),
-        cryptorchidie = factor(cryptorchidie,
-                               levels = c('Oui', 'Non'),
-                               labels = c(1, 0)),
-        fecondite = factor(fecondite,
-                           levels = c('primaire', 'secondaire'))
+        diplome_h = ordered(diplome_h,
+                            levels = c('Bac-', 'Bac', 'Bac+'),
+                            labels = c('Bac--', 'Bac', 'Bac++')),
+        diplome_f = ordered(diplome_f,
+                            levels = c('Bac-', 'Bac', 'Bac+'),
+                            labels = c('Bac--', 'Bac', 'Bac++')),
+        cryptorchidie = ordered(cryptorchidie,
+                                levels = c('Oui', 'Non')),
+        fecondite = ordered(fecondite,
+                            levels = c('primaire', 'secondaire'))
     )
 
 ##==================================================
@@ -94,9 +94,6 @@ all_patho_h
 
 
 # Pathologie
-patho_h_bin <- factor(patho_h != 'non',
-                      levels = c(TRUE, FALSE),
-                      labels = c(1, 0))
 
 v_chimio <- c("chimiotherapie",
               "cancer_testis",
@@ -111,9 +108,12 @@ v_chronic <- c("pathologies_respiratoire_chroniques",
 v_autre <- setdiff(all_patho_h, c("non", v_chimio, v_chronic))
 
 
+patho_h_bin <- ordered(patho_h != 'non',
+                       levels = c(TRUE, FALSE),
+                       labels = c(1, 0))
 
 # Chimiotherapie
-patho_h_regroup = factor(
+patho_h_regroup = ordered(
     ifelse(patho_h == "non", "non",
            ifelse(patho_h %in% v_chimio, "chimio",
                   ifelse(patho_h %in% v_chronic, "chronic", "autre"))),
@@ -121,7 +121,10 @@ patho_h_regroup = factor(
 )
 table(patho_h_regroup)
 
-# 
+
+
+
+
 # patho_h_chimio <- factor(
 #     sapply(patho_h,
 #            contains_values,
@@ -178,13 +181,13 @@ table(patho_f)
 # non  pb tubaire bilateral pb tubaire unilateral 
 # 647                    14                    65
 
-patho_f_bin <- factor(
+patho_f_bin <- ordered(
     (patho_f != 'non'),
     levels = c(TRUE, FALSE),
     labels = c(1, 0)
 )
 
-patho_f_regroup <- factor(
+patho_f_regroup <- ordered(
     ifelse(
         test = is.na(patho_f),
         yes = NA,
