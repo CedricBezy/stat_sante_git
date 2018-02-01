0717f7b086356e6b66506f56e8b7701da1db3984
Unknown <nabil.hb7@gmail.com>
2018-02-01 15:47:37 +0100

    Update Reg Log

--- code/RegressionLogistique.R
+++ code/RegressionLogistique.R
@@ -7,56 +7,20 @@ library(broom)
 
 load("/Users/nabil/Desktop/BESante/couples.RData")
 couples.bis <- couples[,-1]
-
-# Regression Logistique
-reg <- glm(enfant ~ age_h, data = couples.bis, family = binomial(logit)) #+ age_f + diplome_h + diplome_f + bmi_h + patho_h_regroup + cryptorchidie + fecondite +  duree_infertilite + traitement
-summary(reg)
-
-# Coefficient 
-exp(coef(reg))
-# Coefficients & IC
-exp(cbind(coef(reg), confint(reg)))
-# Intervalle de confiance & p-value
-tmp <- tidy(reg,conf.int = TRUE, exponentiate = TRUE)
-str(tmp)
-# Plot les coeufficients avec IC
-ggplot(tmp) + aes(x = estimate, y = term, xmin = conf.low, 
-                  xmax = conf.high) + geom_vline(xintercept = 1) + 
-  geom_errorbarh() + geom_point() + scale_x_log10()
-
-# Anova
-anova(reg, test="Chisq")
-# Pour voir l'effet de chaque variable du modele
-plot(allEffects(reg))
-# Regression descendante 
-res <- step(reg)
-#Odd Ratio
-odds.ratio(reg)
+couples.bis$traitement <- relevel(couples.bis$traitement, "Aucun")
 
 ####
 #                        Construction du model 
-
+#  Sans valeur manquntes
+couples.bis <- subset(couples.bis, !is.na(couples.bis$bilan_f))
 # modèle trivial réduit à la constante
 str_constant <- "~ 1"
 # modèle complet incluant toutes les explicatives potentielles
-str_all <- "~age_h+age_f+diplome_h+bmi_h+duree_infertilite+traitement"
+str_all <- "~ age_h+age_f + diplome_h + bmi_h_class_2 + cryptorchidie + fecondite +bilan_f+duree_infertilite_class+traitement"
 require(MASS)
 
-# Forward
-modele <- glm(enfant ~ 1, data = couples, family = binomial)
-modele.forward <- stepAIC(modele, scope = list(lower = str_constant, upper = str_all), trace = TRUE, data = couples, direction = "forward")
-# affichage du modèle final
-summary(modele.forward)
-# Anova
-anova(modele.forward, test="Chisq")
-
-# Backward
-modele <- glm(paste("enfant",str_all), data =couples.bis , family = binomial)
-modele.backward <- stepAIC(modele, scope = list(lower = str_constant, upper = str_all), 
-                           trace = TRUE, data = couples, direction ="backward")
-summary(modele.backward)
 # Stepwise
-modele <- glm(enfant ~ 1, data = , family = binomial)
+modele <- glm(enfant ~ 1, data =couples.bis , family = binomial)
 modele.stepwise <- stepAIC(modele, scope = list(lower = str_constant, upper = str_all), 
                           trace = TRUE, data = couples, direction = "both")
 # affichage du modèle final
@@ -65,15 +29,49 @@ anova(modele.stepwise, test="Chisq")
 
 tmp <- tidy(modele.stepwise,conf.int = TRUE, exponentiate = TRUE)
 str(tmp)
+
 # Plot les coeufficients avec IC
 ggplot(tmp) + aes(x = estimate, y = term, xmin = conf.low, 
                   xmax = conf.high) + geom_vline(xintercept = 1) + 
   geom_errorbarh() + geom_point() + scale_x_log10()
 
 odds.ratio(modele.stepwise)
-# Validation du modèle : Indicateurs de qualité et de robustesse
-par(mfrow = c(1, 1))
-plot(rstudent(modele.stepwise), type = "p", cex = 0.5, ylab = "Résidus studentisés ", 
-     col = "springgreen2", ylim = c(-3, 3))
-abline(h = c(-2, 2), col = "red")
+plot(allEffects(modele.stepwise))
+# Courbe de ROC
+build_roc(modele.stepwise)
+
+
+
+
+####
+#                        Construction du model 
+#  En considerant les NA's comme une classe
+levels(couples.bis$bilan_f) <- c(levels(couples.bis$bilan_f),"pas_Bilan")
+couples.bis$bilan_f[is.na(couples.bis$bilan_f)] <- "pas_Bilan"
+# modèle trivial réduit à la constante
+str_constant <- "~ 1"
+# modèle complet incluant toutes les explicatives potentielles
+str_all <- "~ age_h+age_f + diplome_h + bmi_h_class_2 + cryptorchidie + fecondite +bilan_f+duree_infertilite_class+traitement"
+require(MASS)
+
+# Stepwise
+modele <- glm(enfant ~ 1, data =couples.bis , family = binomial)
+modele.stepwise <- stepAIC(modele, scope = list(lower = str_constant, upper = str_all), 
+                           trace = TRUE, data = couples, direction = "both")
+# affichage du modèle final
+summary(modele.stepwise)
+anova(modele.stepwise, test="Chisq")
+
+tmp <- tidy(modele.stepwise,conf.int = TRUE, exponentiate = TRUE)
+str(tmp)
+
+# Plot les coeufficients avec IC
+ggplot(tmp) + aes(x = estimate, y = term, xmin = conf.low, 
+                  xmax = conf.high) + geom_vline(xintercept = 1) + 
+  geom_errorbarh() + geom_point() + scale_x_log10()
+
+odds.ratio(modele.stepwise)
+plot(allEffects(modele.stepwise))
+# Courbe de ROC
+build_roc(modele.stepwise)
 
